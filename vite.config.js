242e736b18c992d618832a74547eb52a320bf2dc
zakaria-eltawill <zakariaeltawill@gmail.com>
2026-04-13 16:46:59 +0200

    Fix Vite base path for proper asset loading on deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/CDP_Website/',
+  base: './',
   plugins: [vue()],
   resolve: {
     alias: {
